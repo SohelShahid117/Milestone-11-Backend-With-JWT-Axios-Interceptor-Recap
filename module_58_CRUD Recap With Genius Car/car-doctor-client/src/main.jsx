25d935d744eadd2176379e585dc474857ceb3546
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-16 08:56:10 +0600

    58-1 Module overview and project setup done

---
+++ module_58_CRUD Recap With Genius Car/car-doctor-client/src/main.jsx
@@ -0,0 +1,42 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Main from "./Layout/Main";
+import Home from "./Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <div>Hello world!</div>,
+    element:<Main></Main>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
