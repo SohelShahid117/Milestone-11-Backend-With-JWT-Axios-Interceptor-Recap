dd80f2e5bee0519478b10c5dd3b4c277760dd4bc
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-17 20:16:19 +0600

    done upto 59-2 Insert bulk Data In Database and Load services data

--- module_58_CRUD Recap With Genius Car/car-doctor-client/src/Pages/Home/Services/ServiceCard.jsx
+++ module_58_CRUD Recap With Genius Car/car-doctor-client/src/Pages/Home/Services/ServiceCard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
   // console.log(service)
-  const { title, img, price } = service;
+  const { _id, title, img, price } = service;
+  // console.log(_id);
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <div className=" rounded-xl h-2/3 w-96 mt-5">
@@ -15,12 +17,13 @@ const ServiceCard = ({ service }) => {
             <p className="text-xl font-semibold text-[#FF3811]">
               Price : ${price}
             </p>
-            <a
-              href="#slide5"
+            {/* <Link to="/login"> */}
+            <Link
+              to={`/checkout/${_id}`}
               className="btn btn-circle bg-[#FF3811] border-[#FF3811] text-white hover:bg-[#FF3811] hover:text-white -mt-2"
             >
               ❯
-            </a>
+            </Link>
           </div>
         </div>
       </div>
